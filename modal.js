dbaa830e53224a96dfa0df581446acd100856672
Samaheh Loghmani <>
2022-02-04 16:00:34 -0500

    add more functionality

--- modal.js
+++ modal.js
@@ -2,15 +2,21 @@ const showModal = document.querySelector(".show-modal");
 const modal = document.querySelector(".modal");
 const closeModal = document.querySelector(".close-modal");
 const backDrop = document.querySelector(".backdrop");
+const confirmbtn = document.querySelector(".confirm-modal");
 
 showModal.addEventListener("click", () => {
     modal.style.opacity = 1;
     backDrop.style.display = "block";
     modal.style.transform = "translateY(20vh)";
 });
-
-closeModal.addEventListener("click", () => {
+function closeFunction() {
     modal.style.opacity = 0;
     backDrop.style.display = "none";
     modal.style.transform = "translateY(-100vh)";
+}
+closeModal.addEventListener("click", closeFunction);
+backDrop.addEventListener("click", closeFunction);
+confirmbtn.addEventListener("click", () => {
+    closeFunction();
+    alert("thanks");
 });
